fdad1f3e1f21b2c6adcd3967ab33a9a4acc56bcc
epoxydel <yanis.delmas@iut-rodez.fr>
2019-12-04 09:47:22 +0100

    m

--- ProjetSimulation/src/simulation/LoiExponentielle.java
+++ ProjetSimulation/src/simulation/LoiExponentielle.java
@@ -16,21 +16,26 @@ public class LoiExponentielle {
 	 * @param x , valeur à vérifier
 	 * @return true si x > 0 et false si x < 0 
 	 */
-	public static boolean verifValeur (double x) {
+	public static boolean verifLoisExpo (double x) {
 		return x > 0 ;
 	}
 	
 	/**
-	 * 
+	 * Simulation de la loi exponentielle
 	 * @param x , position 
 	 * @param lambda, paramètre de la lois
 	 * @return le résultat de la lois
 	 * @throws ErreurParametreNegatif 
 	 */
 	public static double loiExpo(double lambda) throws ErreurParametreNegatif {
-		if (!verifValeur(lambda)) {
-			throw new ErreurParametreNegatif();
-		}
+		
+		/* Vérification du paramètre 
+		 *if (!verifLoisExpo(lambda)) {
+		 *	throw new ErreurParametreNegatif();
+		 *}
+		 */
+		
+		/* Simulation */
 		return (-1/lambda)* Math.log(Math.random()) ;
 	}
 	
